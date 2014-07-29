b1a31f5c6ddc71fa10cf61756cca8344379253d8
Alexander Habermann <hehe3301@gmail.com>
2014-07-29 13:21:33 -0400

    Updated config and restart

--- serv_restart.sh
+++ serv_restart.sh
@@ -3,5 +3,7 @@
 # @Author Alex N. Habermann
 # this script restarts a minecraft server by shutting it down, closing
 # the tmux window, reopening the window and starting the server again.
+tumx send-keys -t mineserv "say Server will be restarting in 60s" C-m
+sleep 60s
 sh serv_shutdown.sh
 sh serv_startup.sh
